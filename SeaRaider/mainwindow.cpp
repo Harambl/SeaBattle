1ce3e85992748213738d2ffffe600cd78d8e6fdd
Harambl <aspentya@gmail.com>
2025-12-06 17:46:06 +1000

    Add files via upload

--- SeaRaider/mainwindow.cpp
+++ SeaRaider/mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QInputDialog>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->stackedWidget->setCurrentIndex(0);
+
+    QVBoxLayout* playersLayout = new QVBoxLayout();
+    playersLayout->setContentsMargins(0, 0, 0, 0);
+    playersLayout->setSpacing(2);
+    playersLayout->stretch(0);
+    ui->WidgetContents->setLayout(playersLayout);
+
+    connect(ui->Add_button, &QPushButton::clicked, this, [this, playersLayout](){
+        createPlayer(playersLayout);
+    });
+
 }
 
 MainWindow::~MainWindow()
@@ -13,30 +29,64 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::createPlayer(){
+void MainWindow::createPlayer(QVBoxLayout* Layout){
     players++;
     QString strnumber = QString::number(players);
 
     QWidget* PWidget = new QWidget;
-    PWidget->setStyleSheet("background-color: peru; border-radius: 6px; padding: 5px;");
-    PWidget->setMaximumHeight(80);
+    PWidget->setStyleSheet("background-color: BurlyWood; border-radius: 6px; padding: 5px;");
+    PWidget->setFixedHeight(60);
 
     QLabel* NumLabel = new QLabel();
     NumLabel->setText(strnumber);
 
-    QLabel* NameLabel = new QLabel();
+    QLabel* NameLabel = new QLabel(CurrentUser);
 
     QHBoxLayout* PLayout = new QHBoxLayout(PWidget);
     NumLabel->setStyleSheet("font-weight: bold;");
 
-    QPushButton* compButton = new QPushButton;
-    connect(compButton, &QPushButton::clicked, this, [this](){
+    QPushButton* fButton = new QPushButton("Предложить бой");
+    connect(fButton, &QPushButton::clicked, this, [this](){
 
     });
+    fButton->setStyleSheet("background-color: olivedrab");
 
     PLayout->addWidget(NumLabel);
     PLayout->addWidget(NameLabel);
-    PLayout->addWidget(compButton);
+    PLayout->addWidget(fButton);
 
-    ui->PlayersLayout->addWidget(PWidget);
+    Layout->addWidget(PWidget);
+    if (!hasStretch){
+        Layout->addStretch();
+        hasStretch = true;
+    } else {
+        QLayoutItem *item = Layout->itemAt(players - 1);
+        Layout->takeAt(players - 1);
+        delete item;
+        Layout->addStretch();
+    }
+}
+
+void MainWindow::on_start_button_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_connect_button_clicked(){
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_Log_button_clicked(){
+    QString name = QInputDialog::getText(this, "Вход", "Введите ваше имя: ", QLineEdit::Normal);
+    //QString password = QInputDialog::getText(this, "Вход", "Введите ваш пароль: ", QLineEdit::Normal);
+    if (name != ""){
+        CurrentUser = name;
+        ui->UserLb->setText(name);
+        ui->stackedWidget->setCurrentIndex(3);
+    }
+    else {
+        QMessageBox* errorMessage = new QMessageBox;
+        errorMessage->information(this, "Invalid name", "Введите корректное имя");
+        delete errorMessage;
+    }
 }
--- SeaRaider/mainwindow.h
+++ SeaRaider/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,12 +18,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void createPlayer();
+    void createPlayer(QVBoxLayout* Layout);
+
+private slots:
+
+    void on_start_button_clicked();
+    void on_connect_button_clicked();
+    void on_Log_button_clicked();
 
 private:
     Ui::MainWindow *ui;
 
+    QString Ip = "127.0.0.1";
+    QString Port = "55555";
+
     long long players = 0;
+    QString CurrentUser = "";
+    bool hasStretch = false;
+
 
 };
 #endif // MAINWINDOW_H
